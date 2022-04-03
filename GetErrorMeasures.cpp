bd92bff28c597d868bb35c59b5de085bc3eba232
Indranil Guha <indranil-guha@uiowa.edu>
2022-04-03 10:34:38 -0500

    adding news

---
+++ GetErrorMeasures.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include "CImg.h"
+using namespace cimg_library;
+using namespace std;
+
+string basepath="";
+string errorfilename="";
+
+int main(int argc, char* argv[]){
+
+}
